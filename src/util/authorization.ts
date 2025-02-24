dfd23956d84cc0d61f0cf8d961e5d5c4d53a66e3
Isaque D <isaqueduarte17@gmail.com>
2025-02-24 01:43:04 -0300

    chore: remove log

--- src/util/authorization.ts
+++ src/util/authorization.ts
@@ -1,7 +1,5 @@
 import { sign } from "jsonwebtoken";
 
 export const getAuthorizationHeader = (): string => {
-    console.log(process.env.JWT_SECRET);
-    console.log(process.env.API_SECRET);
     return sign({ user: "My front end app", apiSecret: process.env.API_SECRET }, process.env.JWT_SECRET as string);
 };
