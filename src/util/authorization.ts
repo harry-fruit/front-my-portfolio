76e2d907b342ed19cbd792e656718d11a72d9bc7
Isaque D <isaqueduarte17@gmail.com>
2025-02-24 00:01:56 -0300

    CHORE: LOG

--- src/util/authorization.ts
+++ src/util/authorization.ts
@@ -1,5 +1,7 @@
 import { sign } from "jsonwebtoken";
 
 export const getAuthorizationHeader = (): string => {
+    console.log(process.env.JWT_SECRET);
+    console.log(process.env.API_SECRET);
     return sign({ user: "My front end app", apiSecret: process.env.API_SECRET }, process.env.JWT_SECRET as string);
 };
