5494caacd3160e7a45f71020e2b3923857dbe006
Isaque D <isaqueduarte17@gmail.com>
2025-02-24 01:01:11 -0300

    CHORE: Return to bun

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        ENVIRONMENT: process.env.ENVIRONMENT,
+        API_URL: process.env.API_URL,
+        JWT_SECRET: process.env.JWT_SECRET,
+        API_SECRET: process.env.API_SECRET,
+    }
+};
  
 export default withNextIntl(nextConfig);
