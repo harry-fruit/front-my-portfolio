40042787539b97c9b9110525e09195656fae8af5
Isaque D <isaqueduarte17@gmail.com>
2025-03-03 16:56:07 -0300

    fix: change env var name

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -4,8 +4,8 @@ import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 const sesClient = new SESClient({
   region: "sa-east-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+    accessKeyId: process.env.SES_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.SES_SECRET_ACCESS_KEY as string,
   },
 });
 
